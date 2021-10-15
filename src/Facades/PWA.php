79abfea16f3b119eb58a39185bf90ed4a04efe83
Nur Wachid <nur@wach.id>
2021-10-15 13:49:58 +0700

    register pwa

---
+++ src/Facades/PWA.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Turahe\SEOTools\Facades;
+
+use Illuminate\Support\Facades\Facade;
+
+class PWA extends Facade
+{
+    /**
+     * {@inheritdoc}
+     */
+    protected static function getFacadeAccessor()
+    {
+        return 'pwa';
+    }
+
+}
